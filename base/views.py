1680b06a02221562e65a4c86ec7cfcd4ebdc37ed
Francis Nicole <118891776+Naxiousity@users.noreply.github.com>
2023-06-06 11:36:37 +0800

    Fixed User Authenticate in Sign in

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
-from django.db.models import Q #This is the one that enables search parameters to have the AND/OR functionality
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q 
+from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
@@ -15,6 +17,7 @@ from .forms import RoomForm
 def loginPage(request):
 
     if request.method == 'POST':
+
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -61,6 +64,7 @@ def room(request, pk):
 
     return render(request, 'base/room.html', context) 
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     
@@ -73,9 +77,13 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.user:
+        return HttpResponse("You ain't the owner though GTFO!")
 
     if request.method == 'POST':
         form=RoomForm(request.POST, instance=room)
@@ -87,7 +95,7 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
-
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
