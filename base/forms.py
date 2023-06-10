0a0455083b8a787f02484b6655bde7de8b29e829
Francis Nicole <118891776+Naxiousity@users.noreply.github.com>
2023-06-10 10:48:57 +0800

    Added Static;CSS

--- base/forms.py
+++ base/forms.py
@@ -5,4 +5,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' #It means that whatever is under the model "Room", it will automatically make a form based on whats written there e.g. host, topic, name, desc etc.
-        
+        exclude = ['host', 'participants']
