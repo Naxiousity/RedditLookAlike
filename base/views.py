02bdb30c2b65eed1d5cd304012a8813f638d0e6d
Francis Nicole <118891776+Naxiousity@users.noreply.github.com>
2023-06-20 14:36:34 +0800

    Optimized Mobile Responsiveness

--- base/views.py
+++ base/views.py
@@ -73,7 +73,7 @@ def home(request):
         Q(description__icontains=q)
         ) #Query Sets
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q)) #Can modify to see the activity of the people you follow here ## remember that those are double underscores (__)
 
@@ -194,5 +194,11 @@ def updateUser(request):
     return render(request, 'base/update-user.html', {'form': form})
 
 def topicsPage(request):
-    topics = Topic.objects.filter()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics' : topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
