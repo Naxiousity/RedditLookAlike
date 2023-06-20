ccab9be311747274202916d7d7ca894331383f32
Francis Nicole <118891776+Naxiousity@users.noreply.github.com>
2023-06-20 09:42:56 +0800

    User Edit Profile

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' #It means that whatever is under the model "Room", it will automatically make a form based on whats written there e.g. host, topic, name, desc etc.
         exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
