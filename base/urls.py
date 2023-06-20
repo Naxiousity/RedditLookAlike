e1f0f8703ca0febd98aa70d8df1a7964ce4b654f
Francis Nicole <118891776+Naxiousity@users.noreply.github.com>
2023-06-20 11:46:10 +0800

    Added Mobile Accessibility Pt 1

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
@@ -16,4 +16,6 @@ urlpatterns = [
     path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
 
     path('update-user/', views.updateUser, name="update-user"),
+
+    path('topics/', views.topicsPage, name="topics"),
 ] 
